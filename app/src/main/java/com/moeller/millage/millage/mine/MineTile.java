d561a47b043d2bad0251317625903e82a7a79ea2
satisf <ppmoeller@posteo.net>
2017-06-03 12:18:08 +0200

    added onClick for MineTiles

--- app/src/main/java/com/moeller/millage/millage/mine/MineTile.java
+++ app/src/main/java/com/moeller/millage/millage/mine/MineTile.java
@@ -6,10 +6,12 @@ package com.moeller.millage.millage.mine;
 
 public class MineTile {
 
-    private int depth = 1;
-    private int duration = 5;
-    private final double DEPTH_FACTOR = 1.5;
 
+    private final double DEPTH_FACTOR = 1.5;
+    private final int INITIAL_DURATION = 10;
+
+    private int depth = 1;
+    private int duration = INITIAL_DURATION;
     public MineTile(int depth){
         this.depth = depth;
     }
@@ -23,11 +25,24 @@ public class MineTile {
     }
 
     public void onClick(){
+        this.duration --;
+        awardResouces();
+        if(this.duration == 0){
+            onBreakthrough();
+        }
+    }
+
+    private void onBreakthrough(){
+        this.depth++;
+        this.duration = (int) Math.round(INITIAL_DURATION * this.depth * DEPTH_FACTOR);
+    }
+
+    private void awardResouces(){
 
     }
 
     public String toString(){
-        return Integer.toString(depth);
+        return "D: " + Integer.toString(this.depth) + " [" + Integer.toString(this.duration) + "]";
     }
 
 }
