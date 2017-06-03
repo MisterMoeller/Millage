c0437eaa17210a03ec3cf6bddaadfbce0667b113
satisf <ppmoeller@posteo.net>
2017-06-03 12:43:35 +0200

    add ref  acticity to minetile

--- app/src/main/java/com/moeller/millage/millage/mine/MineTile.java
+++ app/src/main/java/com/moeller/millage/millage/mine/MineTile.java
@@ -1,29 +1,52 @@
 package com.moeller.millage.millage.mine;
 
-/**
- * Created by Philipp on 03.06.2017.
- */
+import android.app.Activity;
 
+/**
+ * represents a single clickable tile in the mine
+ */
 public class MineTile {
 
 
+    // factor used to calculate rewards for clicking and new durations on breakthrough
     private final double DEPTH_FACTOR = 1.5;
+    // initial duration and base of future duration calculation
     private final int INITIAL_DURATION = 10;
 
+    // "level" of the tile
     private int depth = 1;
+    // amount of clicks necessary to breakthrough
     private int duration = INITIAL_DURATION;
-    public MineTile(int depth){
-        this.depth = depth;
+    // reference to the main Activity
+    private Activity activity;
+
+    /**
+     * constructor
+     * @param activity
+     */
+    public MineTile(Activity activity){
+        this.activity = activity;
     }
 
+    /**
+     * Getter Depth
+     * @return
+     */
     public int getDepth() {
         return depth;
     }
 
+    /**
+     * Setter Depth
+     * @return
+     */
     public void setDepth(int depth) {
         this.depth = depth;
     }
 
+    /**
+     * onClick function
+     */
     public void onClick(){
         this.duration --;
         awardResouces();
@@ -32,15 +55,25 @@ public class MineTile {
         }
     }
 
+    /**
+     * used on Breakthrough / Levelup
+     */
     private void onBreakthrough(){
         this.depth++;
         this.duration = (int) Math.round(INITIAL_DURATION * this.depth * DEPTH_FACTOR);
     }
 
+    /**
+     * used in onclick and awards resources based on the depth of the tile
+     */
     private void awardResouces(){
-
+        activity.
     }
 
+    /**
+     * String output for the TextView
+     * @return
+     */
     public String toString(){
         return "D: " + Integer.toString(this.depth) + " [" + Integer.toString(this.duration) + "]";
     }
